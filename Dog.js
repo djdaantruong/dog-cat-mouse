aefc74455b55fa2c1e92dc136ac8ee94d3766742
djdaantruong <daantd.a12017@gmail.com>
2020-04-03 23:52:57 +0700

    sayHi method

--- Dog.js
+++ Dog.js
@@ -1,9 +1,16 @@
-function Dog() {
-	// body...
+var chalk = require('chalk');
+
+function Dog(name) {
+	this.name = name;
 	this.stomach = [];
 }
 
 Dog.prototype.eat = function (Cat) {
-	// body...
 	this.stomach.push(Cat);
-}
+};
+
+Dog.prototype.sayHi = function () {
+	console.log('Hi, I am a Dog. My name is ' + chalk.blue(this.name));
+};   
+
+module.exports = Dog;
